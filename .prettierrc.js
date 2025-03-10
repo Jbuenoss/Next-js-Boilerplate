2c7bfd9582740e5d9588f6212038d657399c0fc7
Joao Bueno <joaobueno121212@gmail.com>
2025-03-10 09:56:17 -0300

    removing tailwind plugin because bug in formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,5 +4,5 @@ module.exports = {
     singleQuote: true,
     printWidth: 100,
     tabWidth: 4,
-    plugins: ['prettier-plugin-tailwindcss'],
+    plugins: [],
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div className="">
-            <h1>Home</h1>
+        <div className="flex min-h-screen min-w-screen items-center flex-col mt-10">
+            <h1 className="font-bold text-3xl">Home</h1>
             <p>Modify here</p>
         </div>
     );
